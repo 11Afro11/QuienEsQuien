054760a65521d5991b951e5628c4061301e8e817
Sergio <scmarquez@correo.ugr.es>
2016-01-21 16:51:48 +0100

    hola

--- include/ConjuntoPersonas.h
+++ include/ConjuntoPersonas.h
@@ -13,26 +13,60 @@ class ConjuntoPersonas{
 	private:
 		vector<Persona> per;
 	public:
+		/**
+		*@brief Constructor con parámetro.
+		**/
 		ConjuntoPersonas(vector<Persona> v);
+		/**
+		*@brief Constructor sin parámetro
+		**/
 		ConjuntoPersonas(){
-
 		}
+		/**
+		*@brief Devuelve la persona que ocupa la posición indice
+		*@param El índice del del personaje al que queremos acceder
+		*@return Devuelve una persona
+		**/
 		Persona get_Persona(int indice){
 			if(indice >= 0  && indice <per.size()){
 				return per[indice];
 			}
 		}
+		/**
+		*@brief Añade una persona al vector de personas
+		*@param Persona que deseamos añadir
+		**/
 		void aniade_Persona(Persona p);
 		//Estos métodos no son esenciales
 		//////////////////////////////
+		/**
+		*@brief Devuelve el nombre de la persona que ocupa el índice indicado en el vector.
+		*@param Índice de la persona
+		*@return Nombre de la persona
+		**/
 		string get_name(int indice);
+		/**
+		*@brief Devuelve el path de la foto del personaje pasado como índice
+		*@param Índice de la persona
+		*@return Path de la foto de la persona
+		**/
 		string get_path(int indice);
 		/////////////////////////////
+		/**
+		*@brief Devuelve el tamaño del conjunto de preguntas
+		*@return Entero con el tamaño de la pregunta
+		**/
+
 		int get_size(){
 			return per.size();
 		}
 
 		//Añadir operator [] const y no const
+		/**
+		*@brief Devuelve la persona en el índice indicado
+		/@param Índice de la persona
+		*@return Persona en el índice indicado
+		**/
 		Persona& operator [](int indice){
 			return per[indice];
 		}
@@ -41,9 +75,29 @@ class ConjuntoPersonas{
 			return get_Persona(indice);
 		}
 		*/
+		/**
+		*@brief Borra la persona que ocupa el índice pasado como parámetro
+		*@param Índice de la pregunta
+		**/
 		void borra_Persona(int indice);
+		/**
+		*@brief Borra la persona que se le pasa como parámetro
+		*@param Persona a borrar
+		**/
 		void del (Persona p);
+		/**
+		*@brief Sobrecarga del operador de entrada
+		*@param &flujo: flujo de entrada
+		*@param &conj: ConjuntoPersonas al que se redirige la entrada
+		*@return flujo de entrada
+		**/
 		friend ostream& operator << (ostream &flujo, ConjuntoPersonas &conj);
+		/**
+		*@brief Sobrecarga del operador de salida
+		*@param &flujo: flujo de salida
+		*@param &conj: ConjuntoPersonas a imprimir
+		*@return devuelve un flujo a  salida
+		**/
 		friend istream& operator >> (istream &flujo, ConjuntoPersonas &conj);
 
 };
--- include/Persona.h
+++ include/Persona.h
@@ -40,24 +40,54 @@ class Persona{
 				return info[codigo];
 			}
 		}
+		/**
+		*@brief Devuelve el nombre de la persona
+		*@return string con el nombre de la persona
+		**/
 		string get_name(){
 			return name;
 		}
+		/**
+		*@brief Devuelve el path de la foto del personaje
+		*@return string con el path de la foto 
+		**/
 		string get_path(){
 			return path_imagen;
 		}
+		/**
+		*@brief Inicializa el nombre de la persona
+		*@param string con el nombre
+		**/		
 		void set_name(string n){
 			name = n;
 		}
+		/**
+		*@brief inicializa el path de la persona
+		*@param string con el path
+		**/
 		void set_path(string p){
 			path_imagen = p;
 		}
+		/**
+		*@brief inicializa el info de la persona
+		*@param vector<int> con la info
+		**/
 		void set_info(vector<int> v){
 			info = v;
 		}
+		/**
+		*@brief Devuelve el elemento i en el vector
+		*@param Indice del vector
+		*@return entero con el elemento del vector
+		**/
 		int& operator [] (int indice){
 			return info[indice];
 		}
+		/**
+		*@brief sobrecarga del operador de asiganación
+		*@param Persona con la que se compara 
+		*@return Devuelve true si son iguales y false en caso contrario
+		**/
 		bool operator == (Persona p){
 			bool iguales = false;
 			if(info == p.info){
@@ -69,7 +99,19 @@ class Persona{
 			}
 			return iguales;
 		}
+		/**
+		*@brief Sobrecarga del operador >> de entrada
+		*@param &flujo: flujo de enttrada
+		*@param &p: Persona a escribir
+		*@return Devuelve una referencia añ flijo de entrada
+		**/
 		friend istream& operator >> (istream &flujo, Persona &p);
+		/**
+		*@brief Sobrecarga del operador de salida
+		*@param &flujo: flujo de salida
+		*@param &p: persona a escribir
+		*@return devuelve una referencia al flujo de salida
+		**/
 		friend ostream& operator << (ostream &flujo, Persona &p);
 
 };
